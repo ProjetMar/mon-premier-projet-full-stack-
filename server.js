853d490b7334bcf841968d9143a08854006eed7e
marwa mezghani <marwamezghani29@gmail.com>
2025-03-12 22:22:47 +0100

    -modifier le server.js avec des fc d'amélioration
    -ajouter un fichier frontend et initialiser mon app react avec npm create vite@latest my-react-app -- --template react
    -mettre tous mon mon backend dans un dossier backend

--- server.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-const app = require('./app');
-// dire à l'app express sur quel port elle va tourner 
-app.set('port', process.env.PORT || 8888);
-const server = http.createServer(app);
-
-server.listen(process.env.PORT || 8888);
